5b52b742063a333d05e83ce73e6a901008e2382b
kendalllewis <kendall.ryan.lewis@gmail.com>
2025-09-05 21:53:03 -0500

    Initial Commit

---
+++ CarnivoreGolf/CarnivoreGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnivoreGolfApp.swift
+//  CarnivoreGolf
+//
+//  Created by Kendall Lewis on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CarnivoreGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarnivoreGolfTests/CarnivoreGolfTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarnivoreGolfTests.swift
+//  CarnivoreGolfTests
+//
+//  Created by Kendall Lewis on 9/5/25.
+//
+
+import Testing
+@testable import CarnivoreGolf
+
+struct CarnivoreGolfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
